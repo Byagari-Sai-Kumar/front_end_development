b8ae18c6ec4b26414641823c1e0cf7dbb2c4f8fa
Byagari-Sai-Kumar <sai815538@gmail.com>
2025-11-18 06:52:14 +0530

    Understanding files and folders, Functional and Class components

--- learning_react/hello-world/src/App.js
+++ learning_react/hello-world/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+//import {Greet} from './components/Greet/greet';
+import Greet from './components/Greet/greet';
+import HelloWorld from './components/HelloWorld/helloWorld';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -18,7 +21,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Greet/>
+      <HelloWorld/>
     </div>
   );
 }
---
+++ learning_react/hello-world/src/components/Greet/greet.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+// function Greet(){
+//     return <h1>Hello Sai kumar. Welcome to React!!</h1>
+// }
+
+//we can also use arrow functions 
+
+const Greet = () => <h1>Hello Sai kumar. Welcome to React!! from Functional component</h1>
+
+//this is a default exporting and when we do this we can use any name for this component while importing into another component.
+export default Greet;
